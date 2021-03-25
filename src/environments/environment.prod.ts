e6cb3e8cf0d30d608bd58d85e3e7a35a8c58bb32
henry <henrymyil@gmail.com>
2021-03-25 11:14:39 +0200

    Valmis Cli

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9WeS6lZInC9IUB_sxsn8bNndNNkMk5EI",
+    authDomain: "hello-cli-c7bc5.firebaseapp.com",
+    projectId: "hello-cli-c7bc5",
+    storageBucket: "hello-cli-c7bc5.appspot.com",
+    messagingSenderId: "843425585523",
+    appId: "1:843425585523:web:2564267abadede5ecb0d06"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9WeS6lZInC9IUB_sxsn8bNndNNkMk5EI",
+    authDomain: "hello-cli-c7bc5.firebaseapp.com",
+    projectId: "hello-cli-c7bc5",
+    storageBucket: "hello-cli-c7bc5.appspot.com",
+    messagingSenderId: "843425585523",
+    appId: "1:843425585523:web:2564267abadede5ecb0d06"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
